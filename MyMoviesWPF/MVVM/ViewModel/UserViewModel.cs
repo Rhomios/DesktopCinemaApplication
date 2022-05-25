a733b75a66e10468d3a16aae0031812294be52e7
Rhomaios <romaios@gmail.com>
2022-05-25 12:48:58 +0700

    ustal

--- MyMoviesWPF/MVVM/ViewModel/UserViewModel.cs
+++ MyMoviesWPF/MVVM/ViewModel/UserViewModel.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using MyMoviesWPF.MVVM.View;
 
 namespace MyMoviesWPF.MVVM.ViewModel
 {
     public class UserViewModel : BaseViewModel
     {
+        public RelayCommand<object> OpenMainSR
+        {
+            get => new RelayCommand<object>(o =>
+            {
+               MainSR sr = new MainSR();
+               sr.Show();
+
+            }
+            );
+        }
+
+        //foreach (Window item in Application.Current.Windows)
+        //    {
+        //        if (item.DataContext == this) item.Close();
+        //    }
+        //public RelayCommand LogInCommand
+        //{
+        //    get { return _view; }
+        //    set
+        //    {
+        //        _view = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
     }
 }
